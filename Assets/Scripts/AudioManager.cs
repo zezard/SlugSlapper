e10e388cbe27bbc21b8f4009124975c1fa00f2b2
Zezard <spam.erik@einebrant.se>
2017-06-09 23:51:40 +0200

    Updated for unity 5.6 and sounds and such

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    public Dictionary<string, AudioClip> sounds;
+
+	// Use this for initialization
+	void Start () {
+        sounds = new Dictionary<string, AudioClip>();
+		AudioSource[] sources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+		foreach (AudioSource source in sources)
+        {
+            sounds.Add(source.name, source.clip);
+            Debug.Log(source.name);
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
