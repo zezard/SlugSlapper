8d1e68c7df55cad3ec5ebd14d05cb62e8fb19689
Zezard <spam.erik@einebrant.se>
2017-06-10 13:55:18 +0200

    Slapping and tapping working on main slug, cleaned scene, added some songs so that the folder isn't empty not used atm

--- Assets/Scripts/SlapBox.cs
+++ Assets/Scripts/SlapBox.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class SlapBox : MonoBehaviour {
 	private MainController _controller;
 	private bool _isSlapping = false;
-	private Stack<float> _distances = new Stack<float>();
+	//private Stack<float> _distances = new Stack<float>(20);
+	private Queue<float> _distances = new Queue<float>();
+	private Vector3 _lastPos;
+	public int distSamples = 20;
+	public float curMeanDist = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,15 +19,32 @@ public class SlapBox : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown("k")) {
-			_controller.OnSlap(this);
+			_controller.OnSlap(this, 0);
 		}
+		if(_lastPos == null) {
+			_lastPos = transform.position;
+		}
+
+		_distances.Enqueue(Vector3.Distance(transform.position, _lastPos));
+		if(_distances.Count > distSamples) {
+			_distances.Dequeue();
+		}
+
+		float totalDistance = 0f;
+		foreach(float dist in _distances) {
+			totalDistance += dist;
+		}
+		curMeanDist = totalDistance / distSamples;
+
+		_lastPos = transform.position;
+
 	}
 
 	//
 	private void OnCollisionEnter(Collision collision) {
 		Debug.Log(collision.gameObject.name);
 		if(collision.gameObject.GetComponent<Slappable>() && !_isSlapping){
-			_controller.OnSlap(this);
+			_controller.OnSlap(this, curMeanDist);
 			_isSlapping = true;
 		}
 	}
