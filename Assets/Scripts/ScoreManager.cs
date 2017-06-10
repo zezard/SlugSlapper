a2d2a11914175ebe514fef97e7ab60d603dda8a4
Lumianna <ninja.tukiainen@gmail.com>
2017-06-10 12:32:25 +0100

    Added a simple score manager.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public Text scoreText;
+	int score;
+
+	public int GetScore() {
+		return score;
+	}
+
+	public void AddScore(int points) {
+		score += points;
+		UpdateScoreText ();
+	}
+
+	private void UpdateScoreText() {
+		scoreText = score;
+	}
+}
